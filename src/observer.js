86d755f2c686d4eb527a0bbb434b64b9ecafc1f1
shaqihe <330630770@qq.com>
2017-03-17 15:25:44 +0800

    @dev observer方法基本完成

--- src/observer.js
+++ src/observer.js
@@ -33,7 +33,29 @@ Oserver.prototype = {
     },
     //转换的主函数
     defineReactive: function (data, key, value) {
-
+        var dep = new Dep();
+        var childObj = observe(val);
+        Object.defineProperty(data, key, {
+            enumerable: true, //可枚举
+            configurable: false, //不再defineProperty
+            get: function () {
+                // 通过Dep定义一个全局target属性，暂存watcher, 添加完移除
+                if (Dep.target) {
+                    dep.depend();
+                }
+                return value;
+            },
+            set: function (newVal) {
+                if (newVal === value) {
+                    return;
+                }
+                value = newVal;
+                //如果value 是一个对象，还要进入监控流程
+                observer(value);
+                //通知订阅者，数据发生改变了
+                dep.notify();
+            }
+        });
     }
 
 }
@@ -41,17 +63,42 @@ Oserver.prototype = {
 
 
 //------------------------要用到发布、订阅，下面简单实现一个事件对象----------------
+// 一般的事件对象如：[{'click1': handleClick1}, {'click2': handleClick2}]
+// handleClick1 是一个函数的引用。 具体是事件模型，和vue 或者 大部分非DOM事件是一个思想
+// vue中，on('xxx')  就是主动往 subs 中push一个事件对象，
+// emit('click1') 时，就是找到 click1对应的handleClick1并且运行
 
+
+var uid = 0; //给每个生成的 Dep 都加一个唯一标识
 function Dep() {
-    // 这个数组来维护所有的，事件对象，如：[{'click1': handleClick1}, {'click2': handleClick2}]
-    // handleClick1 是一个函数的引用。 具体是事件模型，和vue 或者 大部分非DOM事件是一个思想
-    // vue中，on('xxx')  就是主动往 subs 中push一个事件对象，
-    // emit('click1') 时，就是找到 click1对应的handleClick1并且运行
+    //"subs" 是维护一个数组，用来收集订阅者，数据变动触发notify，再调用订阅者的update方法
+    this.id = uid++;
     this.subs = [];
 }
 
 Dep.prototype = {
+    //增加一个订阅者
     addSub: function (target) {
         this.subs.push(target)
     },
+
+    // 通过Dep定义一个全局target属性，暂存watcher, 添加完移除
+    depend: function() {
+        Dep.target.addDep(this);
+    },
+
+    //移除订阅者
+    removeSub: function(sub) {
+        var index = this.subs.indexOf(sub);
+        if (index != -1) {
+            this.subs.splice(index, 1);
+        }
+    },
+
+    //通知订阅者，并触发订阅者的update()的方法
+    notify: function() {
+        this.subs.forEach(function(sub) {
+            sub.update();
+        });
+    }
 }
